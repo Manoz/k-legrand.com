82ae8403ea47a051de9277efbf706f0f553eca7b
Kévin Legrand <manoz@outlook.com>
2020-07-21 03:23:14 +0200

    add coming soon content

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
-import { Link } from 'gatsby'
 
 import Layout from '../components/Layout'
 import SEO from '../components/Seo'
 
-const SecondPage = () => (
+const About = () => (
   <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+    <SEO title="404: Not found" />
+    <div className="max-w-60 px-4 mx-auto text-gray-700 font-light flex flex-col items-center">
+      <h1 className="text-3xl md:text-4 font-medium">About me</h1>
+
+      <p className="mb-4 text-xl font-normal">Coming soon...</p>
+    </div>
   </Layout>
 )
 
-export default SecondPage
+export default About
