9e3db0a90a6f6541dba2c5bc9fd93daa5d88e8e7
Kévin Legrand <manoz@outlook.com>
2020-07-22 18:03:29 +0200

    Did I forgot a shadow? Nooooo

--- tailwind.config.js
+++ tailwind.config.js
@@ -134,6 +134,8 @@ module.exports = {
       sm: '0 1px 2px 0 rgba(0, 0, 0, 0.05)',
       lg:
         '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)',
+      xl:
+        '0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)',
       default: '0 1px 2px 0 rgba(0, 0, 0, 0.2)',
       none: 'none',
     },
